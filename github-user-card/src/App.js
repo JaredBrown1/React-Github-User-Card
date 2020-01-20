2b80df3e706e3f82278f7c04f21e3ab28764caec
Jared Brown <jaredbrown1@hotmail.com>
2020-01-20 14:55:39 -0800

    step 1 finished

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,12 +1,31 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Github User Cards</h1>
-    </div>
-  );
+import Card from "./components/github-card";
+
+class App extends React.Component {
+  state = {
+    users: [],
+    userText: ""
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/JaredBrown1")
+      .then(res => res.json())
+      .then(users => {
+        console.log("fetch: users: ", users);
+        this.setState({ ...this.state });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Github User Cards</h1>
+        <Card />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ github-user-card/src/components/github-card.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "../App.css";
+
+function Card() {
+  return (
+    <div className="github-card">
+      <div className="profile-pic">
+        image
+        <img />
+      </div>
+      <div className="profile-info">
+        card info
+        <h3>name</h3>
+        <p>userName</p>
+        <p>location</p>
+        <p>profile</p>
+        <a>address/url</a>
+        <p>followers</p>
+        <p>following</p>
+        <p>bio</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
