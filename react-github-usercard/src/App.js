db3ad36178e4adb7ea66fad706c71843294cf478
Jared Brown <jaredbrown1@hotmail.com>
2020-03-14 10:08:35 -0700

    data fetched with axios

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./components/Card";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/JaredBrown1")
+      .then(res => {
+        console.log("from CDM Card.js", res);
+        return this.setState({ userCards: [res.data] });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Github User Card</h1>
+        {this.state.users.map(item => {
+          return <Card key={item.id} img={item.avatar_ur} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-github-usercard/src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = props => {
+  return (
+    <div>
+      <img src={props.img} alt="avatar" />
+    </div>
+  );
+};
+
+export default Card;
